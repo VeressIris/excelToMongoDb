c68d6b93b83c4862ecb8349fbf9725074fa737a8
Iris Veress <irisveress@gmail.com>
2024-12-20 16:25:38 +0200

    finished
    TODO: refactor backend code

--- backend/main.py
+++ backend/main.py
@@ -25,14 +25,50 @@ def upload_file():
     
     collection = request.form.get('collection')
     if not collection:
-        return 'No MongoDB link provided', 400
+        return 'No MongoDB collection provided', 400
     
     db_name = request.form.get('db-name')
     if not db_name:
-        return 'No MongoDB link provided', 400
+        return 'No database provided', 400
 
     df = pd.read_excel(file)
     
+    cols = df.columns
+    df = df.reset_index()
+    objects = []
+    for index, row in df.iterrows():
+        obj = {}
+        for col in cols:
+            obj[col] = row[col]
+        objects.append(obj)
+    
+    client = MongoClient(mongodb_link)
+    db = client[db_name]
+    db[collection].insert_many(objects)
+
+    return json.dumps({'success': True}), 200
+
+@app.route('/convertSheets', methods=['POST'])
+def convertSheets():
+    google_sheets_link = request.form.get('sheets-link')
+    if not google_sheets_link:
+        return 'No Google Sheets link provided', 400
+
+    mongodb_link = request.form.get('mongodb-link')
+    if not mongodb_link:
+        return 'No MongoDB link provided', 400
+    
+    collection = request.form.get('collection')
+    if not collection:
+        return 'No MongoDB collection provided', 400
+    
+    db_name = request.form.get('db-name')
+    if not db_name:
+        return 'No database provided', 400
+
+    google_sheets_link = google_sheets_link[:google_sheets_link.find('/edit')]
+    df = pd.read_csv(f'{google_sheets_link}/export?format=csv')
+
     cols = df.columns
     df = df.reset_index()
     objects = []
