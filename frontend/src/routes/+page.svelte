2ce1a0469307beeecc148042fae0e45bb0bb87f1
Iris Veress <irisveress@gmail.com>
2024-12-20 15:22:13 +0200

    added drag&drop

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	let mongodbLink = '';
 	let collection = '';
 	let db = '';
+	let fileMessage = '';
 
 	async function processForm() {
 		if (!file || mongodbLink == '' || collection == '' || db == '') {
@@ -32,6 +33,26 @@
 			console.error(error);
 		}
 	}
+
+	function handleDragOver(event) {
+		event.preventDefault();
+		event.stopPropagation();
+	}
+
+	function handleDragLeave(event) {
+		event.preventDefault();
+		event.stopPropagation();
+	}
+
+	function handleDrop(event) {
+		event.preventDefault();
+		event.stopPropagation();
+		const droppedFiles = event.dataTransfer.files;
+		if (droppedFiles.length > 0) {
+			file = droppedFiles[0];
+			fileMessage = `Selected file: ${file.name}`;
+		}
+	}
 </script>
 
 <div class="flex flex-col items-center justify-center">
@@ -43,21 +64,30 @@
 	>
 		<h3>Upload your file</h3>
 		<p class="mt-2 text-gray-600">File should be an .xls or .xlsx file</p>
-		<label
-			for="file"
+		<div
+			role="group"
+			id="drop-zone"
 			class="duration-250 relative mb-3 mt-6 flex w-full cursor-pointer flex-col items-center justify-center gap-2 rounded-md border-[1.5px] border-black p-3 text-base shadow-[2.5px_3px_0_#000] outline-none transition-shadow hover:shadow-[5.5px_7px_0_#000]"
+			on:dragover={(e) => handleDragOver(e)}
+			on:dragleave={(e) => handleDragLeave(e)}
+			on:drop={(e) => handleDrop(e)}
 		>
-			<span class="text-center text-lg font-bold">Drop file here</span>
-			or
-			<input
-				type="file"
-				name="file"
-				accept=".xls,.xlsx"
-				class="max-w-full file:mr-4 file:rounded-md file:border-0 file:bg-green-50 file:px-4 file:py-2 file:font-semibold hover:file:bg-green-100"
-				required
-				on:change={(e) => (file = e.target.files[0])}
-			/>
-		</label>
+			<p class="text-center text-lg font-bold">Drop file here</p>
+			<p class="mb-3">or click to select a file</p>
+			{#if fileMessage == ''}
+				<input
+					type="file"
+					name="file"
+					accept=".xls,.xlsx"
+					class="block max-w-full file:mr-4 file:rounded-md file:border-0 file:bg-green-50 file:px-4 file:py-2 file:font-semibold hover:file:bg-green-100"
+					required
+					on:change={(e) => (file = e.target.files[0])}
+				/>
+			{:else}
+				<p id="file-name" class="mt-2 text-gray-600">{fileMessage}</p>
+			{/if}
+		</div>
+
 		<div class="mt-4 flex flex-col items-center justify-center">
 			<div class="flex w-full flex-col items-start">
 				<label for="mongodb-link" class="font-bold">MongoDB URI:</label>
@@ -93,7 +123,7 @@
 			</div>
 			<button
 				type="submit"
-				on:click|preventDefault={processForm}
+				on:click={processForm}
 				class="my-2 block w-fit rounded-md bg-green-700 px-3 py-2 text-xl font-semibold text-white outline-green-100 transition-all hover:bg-green-800 hover:outline hover:outline-4 active:bg-green-900"
 				>Upload</button
 			>
